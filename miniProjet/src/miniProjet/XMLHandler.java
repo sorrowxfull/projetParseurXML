db9989e85085c3b994dec36bc83fd3f060f84bdb
Romain Fréchet <hikarirom@gmail.com>
2019-12-17 01:28:10 +0100

    Start of fill of the parser

--- miniProjet/src/miniProjet/XMLHandler.java
+++ miniProjet/src/miniProjet/XMLHandler.java
@@ -7,32 +7,71 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class XMLHandler extends DefaultHandler {
 	
-	boolean inElement = false;
+	String pageARenvoyer;
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-		if(localName == "Id") {
-			System.out.println("Id : " + attributes.getValue("value"));
+//		if(localName == "LesBalises") {
+//			System.out.println("Id : " + attributes.getValue("value"));
+//		}
+//		if(localName == "Nom") {
+//			System.out.println("Nom : " + attributes.getValue("value"));
+//		}
+//		if(localName == "Prenom")
+//		{
+//			System.out.println("Prénom : " + attributes.getValue("value"));
+//		}
+//		if(localName == "Groupe") {
+//			System.out.println("Groupe : " + attributes.getValue("value"));
+//		}
+		switch (localName) {
+			case "LesBalises":
+				pageARenvoyer = pageARenvoyer + "<html>";
+				break;
+			case "Corps":
+				pageARenvoyer = pageARenvoyer + "<body>";
+				break;
+			case "Titre":
+				pageARenvoyer = pageARenvoyer + "<title>" + " " + attributes.getValue("valeur");
+				break;
+			case "Collection":
+				break;
+			case "Paragraphe":
+				break;
+				
 		}
-		if(localName == "Nom") {
-			System.out.println("Nom : " + attributes.getValue("value"));
-		}
-		if(localName == "Prenom")
-		{
-			System.out.println("Prénom : " + attributes.getValue("value"));
-		}
-		if(localName == "Groupe") {
-			System.out.println("Groupe : " + attributes.getValue("value"));
-		}
-		//System.out.println(uri + "/" + localName + "/" + qName + "/" + attributes.getType(0));
 		
 	}
 
 	public void endElement(String uri, String localName, String qName) throws SAXException {
+		switch (localName) {
+			case "LesBalises":
+				pageARenvoyer = pageARenvoyer + "</html>";
+				break;
+			case "Corps":
+				pageARenvoyer = pageARenvoyer + "</body>";
+				break;
+			case "Titre":
+				pageARenvoyer = pageARenvoyer + "</title>";
+				break;
+			case "Collection":
+				break;
+			case "Paragraphe":
+				break;
+			
+		}
 	}
 
 	public void characters(char ch[], int start, int length) throws SAXException {
 		
+	}
+
+	public String getPageARenvoyer() {
+		return pageARenvoyer;
+	}
+
+	public void setPageARenvoyer(String pageARenvoyer) {
+		this.pageARenvoyer = pageARenvoyer;
 	}
 }
 
