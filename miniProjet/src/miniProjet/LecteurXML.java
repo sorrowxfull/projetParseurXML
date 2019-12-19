4218458a73511fbf4e09b0f229bcbafe4124ee7d
Romain Fréchet <hikarirom@gmail.com>
2019-12-19 14:10:04 +0100

    Modify the file to make appear all the students

--- miniProjet/src/miniProjet/LecteurXML.java
+++
@@ -1,5 +0,0 @@
-package miniProjet;
-
-public class LecteurXML {
-
-}
---
+++ miniProjet/src/miniProjet/LecteurXMLEtudiants.java
@@ -0,0 +1,17 @@
+package miniProjet;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class LecteurXMLEtudiants extends DefaultHandler{
+	
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		
+	}
+	public void endElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		
+	}
+
+}
